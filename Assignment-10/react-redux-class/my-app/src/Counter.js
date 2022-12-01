f4d7021791073edd5f0a8d2d64734f69638f7491
steja108 <113944211+steja108@users.noreply.github.com>
2022-11-30 23:38:30 -0500

    Add files via upload

---
+++ Assignment-10/react-redux-class/my-app/src/Counter.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { increment, decrement, reset } from './action';
+
+
+class Counter extends Component {
+    render () {
+
+        const {counter, increment, decrement, reset} = this.props;
+
+        return (
+            <div className="App">
+                <div>Counter {counter}</div>
+                <div><button onClick={increment}>Increment</button></div>
+                <div><button onClick={decrement}>Decrement</button></div>
+                <div><button onClick={reset}>Reset</button></div>
+            </div>
+        )
+    }
+}
+
+const mapStateToprops = (state) => {
+    return {
+        counter: state
+    }
+}
+
+const mapDispatcherToProps = (dispatch) => {
+    return {
+        increment: () => dispatch(increment()),
+        decrement: () => dispatch(decrement()),
+        reset: () => dispatch(reset()),
+    }
+}
+
+
+export default connect(mapStateToprops, mapDispatcherToProps)(Counter);
---
+++ Assignment-10/react-redux-class/my-app/src/action/index.js
@@ -0,0 +1,17 @@
+export const increment = () => {
+    return {
+        type: 'Increment'
+    }
+}
+
+export const decrement = () => {
+    return {
+        type: 'Decrement'
+    }
+}
+
+export const reset = () => {
+    return {
+        type: 'Reset'
+    }
+}
